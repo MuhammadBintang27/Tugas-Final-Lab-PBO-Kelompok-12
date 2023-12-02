f45ce8f46bb0bd9e3c6b840526eb21da20b7765e
MuhammadBintang27 <133251460+MuhammadBintang27@users.noreply.github.com>
2023-12-02 14:23:53 +0700

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -1,17 +1,31 @@
+import java.util.Scanner;
+
 /**
  * Kelas {@code Bank} merupakan subkelas dari {@code Pembayaran} yang mengimplementasikan
  * metode abstrak {@code prosesPembayaran()} untuk menangani pembayaran melalui bank.
  * @author Alvia
  */
 public class Bank extends Pembayaran {
-
      /**
      * Metode ini diimplementasikan untuk menangani proses pembayaran melalui bank.
      * Menampilkan informasi rekening untuk transfer dan memberi konfirmasi setelah transfer diterima.
      */
     @Override
     protected void prosesPembayaran() {
-        System.out.println("     \nSilakan transfer ke No.Rekening 36248245");
-        System.out.println("Pembayaran berhasil setelah transfer diterima Admin\n");
+        Scanner scanner = new Scanner(System.in);
+        String nomorRekeningPembayaran = "1200495522";
+
+        System.out.println("=== Panduan Pembayaran Melalui Bank ===");
+        System.out.println("1. Nomor rekening pembayaran: " + nomorRekeningPembayaran);
+        System.out.println("2. Masuk ke menu transfer");
+        System.out.println("3. Masukkan nomor rekening tujuan");
+        System.out.println("4. Masukkan jumlah pembayaran");
+        System.out.println("5. Masukkan PIN Anda");
+        System.out.println("6. Konfirmasi dan kirim pembayaran");
+        System.out.println("7. Tunggu hingga muncul konfirmasi pembayaran berhasil");
+        System.out.println("Klik 1 jika sudah melakukan pembayaran");
+        int input = scanner.nextInt();
+        System.out.println("\n Pembayaran dengan QRIS sedang diproses");
+        System.out.println("      Menunggu konfirmasi admin\n");
     }
 }
